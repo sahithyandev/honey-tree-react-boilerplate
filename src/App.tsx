3c0a0be4a4ec4b20cf5e58f27ac26744e1be3b46
Sahithyan <sahithyan2701@gmail.com>
2020-12-24 07:51:40 +0530

    got it working

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+// Import CSS like this
+import "./style/main.css";
+
+// Import PNG like this
+import logo from "./assets/logo.png";
+
+// Import SVG like this
+// import logo from "./assets/logo.svg";
+
+export const App = () => {
+  // const;
+
+  return (
+    <div>
+      <img src={logo} className="logo rotate" />
+      <h2>Hi!!!</h2>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
